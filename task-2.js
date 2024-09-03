68bf46ffcb460c36e11a92b5d8f3f93acfb45761
zahra Rafieirad <zahra.rafieirad@dci-student.org>
2024-09-03 15:34:13 +0200

    task completed

--- task-2.js
+++ task-2.js
@@ -10,7 +10,7 @@
 const biscuits = 33;
 const friends = 4;
 
-const biscuitsPerPerson = Math.floor(8);
+const biscuitsPerPerson = Math.floor(biscuits/friends);
 
 // don't change this
 console.log(`Each person can eat ${biscuitsPerPerson} biscuits`);
